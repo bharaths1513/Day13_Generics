73ce9983f03ab8c6f9af0389a8997c2e6546fb94
bharaths1513 <bharaths.1311@gmail.com>
2021-11-11 11:54:02 +0530

    added: Maximum Integer

---
+++ src/Day13_Generics/MaximumTest.java
@@ -0,0 +1,18 @@
+package Day13_Generics;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MaximumTest {
+	@Test
+	public void intMax1() {
+		int actualValue = Maximum.getmaximum(12, 14, 15);
+		Assert.assertEquals(15, actualValue);
+	}
+
+	@Test
+	public void intMax2() {
+		int actualValue = Maximum.getmaximum(1000, 545, 746);
+		Assert.assertEquals(1000, actualValue);
+	}
+}
