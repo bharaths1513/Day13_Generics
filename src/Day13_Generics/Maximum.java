a0f0186e2b0629a587df6636de6fea791c0a0397
bharaths1513 <bharaths.1311@gmail.com>
2021-11-11 12:24:27 +0530

    added: Maximum between Three Integer

--- src/Day13_Generics/Maximum.java
+++ src/Day13_Generics/Maximum.java
@@ -2,7 +2,18 @@ package Day13_Generics;
 
 public class Maximum {
 
-	public static Float getmaximum(Float value1, Float value2, Float value3) {
+	public static Integer getmaximuminteger(Integer value1, Integer value2, Integer value3) {
+		Integer max = value1;
+		if (value2.compareTo(value1) > 0 && value2.compareTo(value3) > 0)
+			max = value2;
+		else if (value3.compareTo(value2) > 0 && value3.compareTo(value1) > 0)
+			max = value3;
+		System.out.println(max);
+
+		return max;
+	}
+
+	public static Float getmaximumfloat(Float value1, Float value2, Float value3) {
 		Float max = value1;
 		if (value2.compareTo(value1) > 0 && value2.compareTo(value3) > 0)
 			max = value2;
--- src/Day13_Generics/MaximumTest.java
+++ src/Day13_Generics/MaximumTest.java
@@ -5,14 +5,26 @@ import org.junit.Test;
 
 public class MaximumTest {
 	@Test
-	public void intMax1() {
-		Float actualValue = Maximum.getmaximum(12f, 14f, 15f);
+	public void intmax1() {
+		Integer actualValue = Maximum.getmaximuminteger(90, 42, 10);
+		Assert.assertEquals(90, actualValue, 0);
+	}
+
+	@Test
+	public void intmax2() {
+		Integer actualValue = Maximum.getmaximuminteger(10, 70, 50);
+		Assert.assertEquals(70, actualValue, 0);
+	}
+
+	@Test
+	public void floatMax1() {
+		Float actualValue = Maximum.getmaximumfloat(12f, 14f, 15f);
 		Assert.assertEquals(15, actualValue, 0);
 	}
 
 	@Test
-	public void intMax2() {
-		Float actualValue = Maximum.getmaximum(1000f, 545f, 746f);
+	public void floatMax2() {
+		Float actualValue = Maximum.getmaximumfloat(1000f, 545f, 746f);
 		Assert.assertEquals(1000, actualValue, 0);
 	}
 }
